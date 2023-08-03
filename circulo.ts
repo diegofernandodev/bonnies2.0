26a493360622fb8b6a3e25198005230e2d46e6b5
Luis Martinez <martinesj405@gmail.com>
2023-08-02 20:54:54 -0500

    circulo

--- circulo.ts
+++ circulo.ts
@@ -1,5 +1,38 @@
-class Circulo {
-    constructor(parameters) {
+export class Circulo {
+   
+
+private radio: number;
+
+
+
+constructor (radio: number){
+  this.radio = radio;
+}
+
+public calcularAreaCirculo(): number {
+    return Math.PI * Math.pow(this.radio, 2);
+  }
+
+  public calcularPerimetroCirculo(): number {
+    return 2 * Math.PI * this.radio;
+  }
+
+       
+    
+    public getradio(): number {
+        return this.radio;
+    }
+    
+    public setradio(value: number) {
+           this.radio=value;
         
     }
+
 }
+let circulo1: Circulo = new Circulo (5)
+
+
+console.log("Área del círculo:", circulo1.calcularAreaCirculo() );
+
+console.log("Perímetro del círculo:",circulo1.calcularPerimetroCirculo());
+
