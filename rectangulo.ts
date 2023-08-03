63cb6ddf7c7e159a084450eeaf2ef23b1352b0b8
danielamosquera <karoldanielamosquera@gmail.com>
2023-08-02 20:42:27 -0500

    rectangulo

--- rectangulo.ts
+++ rectangulo.ts
@@ -1,5 +1,29 @@
-class Rectangulo {
-    constructor(parameters) {
+export class Rectangulo {
+    private base:number
+    private altura:number
+    constructor(base:number, altura:number) {
+        this.base = base
+        this.altura = altura
         
     }
+    public getBase(){
+        return this.base
 }
+    public getAltura(){
+        return this.altura
+    }
+    public setBase(base:number){
+            this.base = base
+}
+    public setAltura(altura:number){
+            this.altura = altura
+}
+    public calcularArea(){
+        return this.base * this.altura
+}
+    
+    public calcularPerimetron(){  
+        return 2*(this.base + this.altura)
+}
+}
+
