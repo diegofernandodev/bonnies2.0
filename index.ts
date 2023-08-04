9e5d0d38e1a9ff22fef21541196759d3dcd7263f
diegofernandodev <dieferv@gmail.com>
2023-08-03 22:24:00 -0500

    se agrego console.table para tabular los objetos

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ console.log("*******************************************************************
 console.log("***************************CUADRADO********************************");
 console.log("*******************************************************************");
 let cuadrado1 = new Cuadrado (8)
+console.table(cuadrado1)
 console.log(`Área de un cuadrado: ${cuadrado1.calculararea()}`);
 console.log(`Perímetro de un cuadrado: ${cuadrado1.calcularperimetro()}`);
 
@@ -12,6 +13,7 @@ console.log("**************************RECTANGULO*******************************
 console.log("*******************************************************************");
 
 let rectangulo1 = new Rectangulo (21,24)
+console.table(rectangulo1)
 console.log(`Área de un rectangulo: ${rectangulo1.calcularArea()}`);
 console.log(`Perímetro de un rectangulo: ${rectangulo1.calcularPerimetro()}`);
 
@@ -21,6 +23,7 @@ console.log("****************************CIRCULO********************************
 console.log("*******************************************************************");
 
 let circulo1 = new Circulo (12)
+console.table(circulo1)
 console.log(`Área de un circulo: ${circulo1.obtenerAreaConDosDecimales()}`);
 console.log(`Perímetro de un circulo: ${circulo1.obtenerPerimetroConDosDecimales()}`);
 
@@ -30,6 +33,7 @@ console.log("*****************************ROMBO*********************************
 console.log("*******************************************************************");
 
 let rombo1 = new Rombo (25,32)
+console.table(rombo1)
 console.log(`Área de un rombo: ${rombo1.calcularArea()}`);
 console.log(`Perímetro de un rombo: ${rombo1.obtenerPerimetroConDosDecimales()}`);
 
@@ -39,6 +43,7 @@ console.log("***************************TRAPECIO********************************
 console.log("*******************************************************************");
 
 let trapecio1 = new Trapecio (12,21,22,32)
+console.table(trapecio1)
 console.log(`Área del trapecio: ${trapecio1.calcularArea()}`);
 console.log(`Perímetro del trapecio: ${trapecio1.calcularPerimetro()}`);
 
@@ -49,7 +54,7 @@ console.log("*********************TRIANGULO RECTANGULO**************************
 console.log("*******************************************************************");
 
 let trianguloRectangulo1 = new TrianguloRectangulo(12,4)
-
+console.table(trianguloRectangulo1)
 console.log(`Área de un triangulo Rectangulo: ${trianguloRectangulo1.calcularArea()}`);
 console.log(`Perímetro de un triangulo Rectangulo: ${trianguloRectangulo1.obtenerPerimetroConDosDecimales()}`);
 console.log(`Hipotenusa de un triangulo Rectangulo: ${trianguloRectangulo1.obtenerHipotenusaConDosDecimales()}`);
